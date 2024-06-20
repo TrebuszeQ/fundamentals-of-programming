dc1d4246d98ea24e40b1be3eaeb2e4c17819d2e1
TrebuszeQ <95846473+TrebuszeQ@users.noreply.github.com>
2024-06-21 01:07:57 +0200

    Update main.cpp

--- student_journal/main.cpp
+++ student_journal/main.cpp
@@ -12,6 +12,22 @@ struct Student {
     string ocena_koncowa;
 };
 
+string read_input_str(const string& msg) {
+    string in;
+    cout << msg << endl;
+    getline(std::cin, in);
+
+    return in;
+}
+
+string str_to_lowercase(const string& str) {
+    string result;
+    for(char cha : str) {
+        result += tolower(cha);
+    }
+    return result;
+}
+
 list<string> open_or_create_file_and_read_to_list(const string&path) {
     list<string> lines;
     ifstream file;
@@ -38,8 +54,9 @@ list<string> open_or_create_file_and_read_to_list(const string&path) {
     try {
         while(getline(file, line)) {
             int line_len = line.length();
-            line = line.substr(0, line_len - 1);
+            line = line.substr(0, line_len);
             printf("Odczytana linia: %s\n", line.c_str());
+            lines.push_back(line);
         }
     }
     catch (exception& e) {
@@ -79,13 +96,13 @@ void write_students_list_to_file(const string& path, const list<Student>& studen
     
     try {
         for(auto student : students) {
-            file << student.tozsamosc << "/r";
+            file << student.tozsamosc << "|";
             for(auto ocena : student.oceny) {
                 file << ocena.c_str() << " ";
             }
-            file << "/r";
-            file << student.srednia_ocen << "/r";
-            file << student.ocena_koncowa << "/r";
+            file << "|";
+            file << student.srednia_ocen << "|";
+            file << student.ocena_koncowa << "\r";
         }
     }
     catch (exception& e) {
@@ -120,8 +137,8 @@ list<string> split_string_by_char(const string& line, const char& delimeter) {
             end = find_char_position(line2, delimeter);
             int line2_len = line2.length();
             if(end == -2) {
-                part = line2;
-                parts_list.push_back(part);
+                // cout << "line2: " << line2 << endl;
+                parts_list.push_back(line2);
                 break;
             }
             else {
@@ -156,26 +173,29 @@ list<int> str_line_to_int_list(const string& line) {
 }
 
 vector<string> list_to_vector(const list<string>& lis, int len) {
-    auto result = vector<string>(len, "");
+    auto result = vector<string>();
     for(const string& node : lis) {
+        // cout << "node: " << node << endl;
         result.push_back(node);
     }
+    // cout << result.size();
     return result;
 }
 
-double count_average_grade(vector<string> grades_v_str) {
+double count_average_grade(const list<int>& grades_list_int) {
     double result = 0;
-    for(string grade : grades_v_str) {
+    for(int grade : grades_list_int) {
         try {
-            result += stoi(grade);
+            // cout << grade;
+            result += grade;
         }
         catch(exception& e) {
-            printf("Wystapil blad podczas koneersji %s na typ integer\n", grade.c_str());
+            printf("Wystapil blad podczas konwersji %i na typ double\n", grade);
         }
     }
 
-    if(result > 0 and grades_v_str.size() > 0) {
-        result /= grades_v_str.size();
+    if(result > 0 and grades_list_int.size() > 0) {
+        result /= grades_list_int.size();
     }
     return result;
 };
@@ -220,32 +240,37 @@ list<Student> create_student_struct_list(list<string>& student_list) {
     list<string> line_splitted;
     list<string> grades_list;
     for(string& line : student_list) {
+        // cout << "line: " << line << endl;
         line_splitted = split_string_by_char(line, '|');
-        grades_str =  line_splitted.back();
+        grades_str = line_splitted.back();
+        // cout << "grades_str: " << grades_str << endl;
         list<string> grades_list = list<string>(split_string_by_char(grades_str, ' '));
-        str_line_to_int_list(line_splitted.back());
+        list<int> grades_int_list = str_line_to_int_list(grades_str);
         Student stud = *new Student;
         stud.tozsamosc = (line_splitted.front());
         stud.oceny = list_to_vector(grades_list, grades_list.size());
-        stud.srednia_ocen = count_average_grade(stud.oceny);
+        stud.srednia_ocen = count_average_grade(grades_int_list);
         stud.ocena_koncowa = determine_grade(stud.srednia_ocen);  
         list_students_struct.push_back(stud);
     }
 
     return list_students_struct;
 };
+void print_student_data(Student& student) {
+    printf("Student: %s\n", student.tozsamosc.c_str());
+    cout << "oceny: ";
+    for(auto ocena : student.oceny) {
+        cout << ocena << " ";
+    }
+    cout << endl;
+    printf("Srednia ocen studenta: %f\n", student.srednia_ocen);
+    printf("Ocena koncowa studenta: %s\n", student.ocena_koncowa.c_str());
+    cout << endl;
+}
 
 void print_students_struct_list(list<Student>& students_list) {
     for(auto student : students_list) {
-        printf("Student: %s\n", student.tozsamosc.c_str());
-        cout << "oceny: ";
-        for(auto ocena : student.oceny) {
-            cout << ocena << " ";
-        }
-        cout << endl;
-        printf("Srednia ocen studenta: %f\n", student.srednia_ocen);
-        printf("Ocena koncowa studenta: %s\n", student.ocena_koncowa.c_str());
-        cout << endl;
+        print_student_data(student);
     }
 }
 
@@ -267,15 +292,42 @@ list<Student> students_with_above_average(const list<Student>& students, double 
     return results;
 }
 
+void print_particular_student(const list<Student>& students) {
+    cout << "Podaj litere x aby wyjsc.\n";
+    string student_name = "";
+    while(student_name != "x") {
+        string student_name = read_input_str("Podaj imie i nazwisko studenta (wielkosc liter nie ma znaczenia): \n");
+        student_name = str_to_lowercase(student_name);
+        int count = 0;
+        for(auto student : students) {
+            string student_id = str_to_lowercase(student.tozsamosc);
+            if(student_id == student_name) {
+                print_student_data(student);
+                break;
+            }
+            count++;
+            if(count == students.size()) {
+                printf("Nie odnaleziono studenta %s\n", student_name.c_str());
+                break;
+            }
+        }
+        cout << endl;
+    }
+    printf("Wyjscie\n");
+}
+
 int main() {
     list<string> students_list = open_or_create_file_and_read_to_list("dane.txt");
     list<Student> students_struct_list = create_student_struct_list(students_list);
     print_students_struct_list(students_struct_list);
     double average_sum = count_average_of_all(students_struct_list);
+    cout << endl;
     printf("Suma srednich wszystkich studentow wynosi %f\n", average_sum);
     list<Student> students_above_average_list =         students_with_above_average(students_struct_list, average_sum);
     printf("Ilosc studentow z srednia ocen powyzej sredniej wynosi %li\n", students_above_average_list.size());
+    cout << endl;
     print_students_above_average(students_above_average_list);
     write_students_list_to_file("studenci_ze_srednia_powyzej_sredniej.txt", students_above_average_list);
+    print_particular_student(students_struct_list);
     return 0;
 }
